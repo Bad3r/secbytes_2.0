85cd84deacb23f0f3f771c5640d4ea78d869fbc8
Bad3r <bad3r@protonmail.com>
2020-02-11 23:46:36 -0500

    changed blog theme

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,7 +7,6 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---node-modules-narative-gatsby-theme-novela-src-templates-articles-template-tsx": hot(preferDefault(require("/home/bad3r/git/secbytes_2.0/node_modules/@narative/gatsby-theme-novela/src/templates/articles.template.tsx"))),
   "component---node-modules-narative-gatsby-theme-novela-src-templates-article-template-tsx": hot(preferDefault(require("/home/bad3r/git/secbytes_2.0/node_modules/@narative/gatsby-theme-novela/src/templates/article.template.tsx"))),
-  "component---node-modules-narative-gatsby-theme-novela-src-templates-author-template-tsx": hot(preferDefault(require("/home/bad3r/git/secbytes_2.0/node_modules/@narative/gatsby-theme-novela/src/templates/author.template.tsx"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/bad3r/git/secbytes_2.0/.cache/dev-404-page.js")))
+  "component---node-modules-narative-gatsby-theme-novela-src-templates-author-template-tsx": hot(preferDefault(require("/home/bad3r/git/secbytes_2.0/node_modules/@narative/gatsby-theme-novela/src/templates/author.template.tsx")))
 }
 
